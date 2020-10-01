8143a4aa13f5f390cbbb81268d790754c54286ae
anand722000 <59029929+anand722000@users.noreply.github.com>
2020-10-01 14:02:37 +0530

    Update remove_vovel.py

--- remove_vovel.py
+++ remove_vovel.py
@@ -1,12 +1,13 @@
-def disemwovel(n):
+#this code will remove all vowels from string
+def remove_vowel(n):
     string=""
-    for i in tuple(n):
+    for i in tuple(n):  #converting string in tuple
         if i not in ['a','e','i','o','u','A','E','I','O','U']:
             string=string+i
     print(string)
 
 n=input()
-disemwovel(n)
+remove_vowel(n)
 
 
 
